c39c7a12949ae79dea2b93f332264436b94a14af
prinnnnnn <106861903+prinnnnnn@users.noreply.github.com>
2024-11-05 22:01:35 +0700

    example of env

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,6 @@ export default async function middleware(req: NextRequest) {
   if (isProtectedRoute) {
     const token = cookies().get('token')?.value;
 
-
     if (!token) {
       return NextResponse.redirect(new URL('/login', req.nextUrl))
     }
