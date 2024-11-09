23b6b7c43f743a31f0374590835c652d2b604d06
prinnnnnn <106861903+prinnnnnn@users.noreply.github.com>
2024-11-09 23:54:46 +0700

    initial mvvm

---
+++ src/app/home-mvvm/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useTheme } from "@/app/ui/ThemeContext";
+import ToggleThemeButton from "@/app/ui/ToggleThemeButton";
+import PeopleWidget from "@/app/ui/PeopleWidget";
+import SideNav from "@/app/ui/SideNav";
+import TrendsWidget from "@/app/home/TrendsWidget";
+import SearchBar from "../home/SearchBar";
+import PostForm from "../home/PostForm";
+
+const HomePage = () => {
+    const { palette } = useTheme();
+    return (
+        <div className="flex flex-row justify-center w-full">
+            <div className="absolute top-2 md:top-5 right-2 md:right-5">
+                <ToggleThemeButton />
+            </div>
+            <div className="flex flex-row justify-evenly md:justify-between w-full md:w-10/12 gap-0 sm:gap-5 pt-10">
+                {/* Navigation Bar */}
+                <SideNav />
+
+                {/* feeds, Form for create post, posts of the followers */}
+                <div
+                    className={`basis-5/6 md:basis-1/2 px-6 py-6 rounded-lg`}
+                    style={{ background: palette.bgSecondary }}
+                >
+                    <SearchBar />
+                    <PostForm />
+                    {/* {store?.homeFeeds.map(post => (
+                        <PostWidget post={post} />
+                    ))} */}
+                </div>
+
+                {/* Trending, who to follow  */}
+                <div
+                    className={`flex-grow hidden md:block rounded-lg px-6 py-6 h-fit`}
+                    style={{ background: palette.bgSecondary }}
+                >
+                    <div className="flex flex-col gap-4">
+                        <TrendsWidget />
+                        <PeopleWidget />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
